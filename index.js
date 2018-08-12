93a02540efe27d79fc64ac1b9faf3d0ce5ad3c07
deeasuquo@gmail.com <deeasuquo@gmail.com>
2018-08-12 19:40:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+  return [...kittens,name]
+}
+function prependKitten(name){
+  return [name,...kittens]
+}
+function removeLastKitten(){
+  return[0,kitten.length-1]
+}
